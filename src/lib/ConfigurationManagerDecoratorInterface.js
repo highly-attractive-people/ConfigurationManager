c60d467ac9c35c7f3e1aad3b3c6eff6ee808d7d5
Helior Colorado <me@helior.info>
2018-10-01 17:07:12 -0700

    Add Decorator interface

---
+++ src/lib/ConfigurationManagerDecoratorInterface.js
@@ -0,0 +1,23 @@
+const ConfigurationManagerInterface = require('./ConfigurationManagerInterface');
+
+/**
+ * Configuration Manager Decorator Interface.
+ *
+ * This should contain a property "component" which conforms to
+ * ConfigurationManagerInterface. Albeit, Javascript is a dynamic language, we
+ * cannot enforce class types on compile time. Likewise, this definition also
+ * serves as an abstract class which is implementing the constructor on behalf
+ * of concrete decorators.
+ *
+ * @extends ConfigurationManagerInterface
+ */
+class ConfigurationManagerDecoratorInterface extends ConfigurationManagerInterface {
+  constructor(component) {
+    if (!component) {
+      throw new Error('You must provide an object that implements to ConfigurationManagerInterface');
+    }
+    this.component = component;
+  }
+}
+
+module.exports = ConfigurationManagerDecoratorInterface;
