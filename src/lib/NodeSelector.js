8c6b2dc1ba39818f396b1ac7ff9379bff5fe3b7b
Helior Colorado <me@helior.info>
2018-10-02 16:53:13 -0700

    Add class/interface for querying a tree structure

---
+++ src/lib/NodeSelector.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const TreeSelector = require('./TreeSelector');
+
+/**
+ * Node Selector traverses a tree structure and fetches the value of a node,
+ * specified by the provided Xpath-like string representation.
+ */
+class NodeSelector extends TreeSelector {
+  /**
+   * Query a tree structure provided a XPath-like string.
+   *
+   * @param  {object} tree
+   * @param  {string} property
+   * @return {Mixed}
+   */
+  static query(tree, property) {
+    if(tree === null || tree === undefined){
+      throw new Error('Calling NodeSelector.query() with null or undefined tree argument');
+    }
+
+    if(property === null || property === undefined){
+      throw new Error('Calling NodeSelector.query() with null or undefined property argument');
+    }
+
+    var value = _query(tree, property);
+
+    if (value === undefined) {
+      throw new Error('Property "' + property + '" is not defined.');
+    }
+
+    return value;
+  }
+}
+
+/**
+ * Private helper function to iterate over tree recursively.
+ *
+ * @param  {object} tree
+ * @param  {string} property
+ * @return {Mixed}
+ */
+function _query(tree, property) {
+  let propertyNameParts = Array.isArray(property) ? property : property.split('.'),
+      name = propertyNameParts[0],
+      value = tree[name];
+
+  if (propertyNameParts.length <= 1) {
+    return value;
+  }
+
+  // Note that typeof null === 'object'
+  if (value === null || typeof value !== 'object') {
+    return undefined;
+  }
+
+  return _query(value, propertyNameParts.slice(1));
+}
+
+module.exports = NodeSelector;
---
+++ src/lib/TreeSelector.js
@@ -0,0 +1,9 @@
+'use strict';
+
+class TreeSelector {
+  static query(tree, property) {
+    throw new Error('You must provide an implementation for the query method.');
+  }
+}
+
+module.exports = TreeSelector;
