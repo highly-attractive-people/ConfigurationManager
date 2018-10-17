f25cc53dc8ecc94e6275491378efa5073e3fe68c
Helior Colorado <me@helior.info>
2018-10-17 13:50:56 -0700

    Minor updates to Cache class

--- src/lib/CacheConfigurationManagerInterface.js
+++ src/lib/CacheConfigurationManagerInterface.js
@@ -8,12 +8,21 @@ const colors = require('colors');
 
 const cacheFileName ='config/CacheConfigurationManager.db.json';
 const defaultOptions = {
-  ttl: 3000
+  ttl: 3000,
+  cacheConfigPath: 'cacheConfigurationManager'
 };
 
 // TODO: reset ttl when reading component config, as this may contain an updated TTL.
 //  - define default config path
-//  - call this.component.get(cache.config.path) to reset tll on this.getRebuiltCache
+//  - call this.component.get(cache.config.path) to reset tll on this.getRebuiltCacheObject
+//  - Define a Cache object instead of using a data object and assuming properties?
+//    - Cache.getData()
+//    - Cache.getLastModified()
+//    - Cache.persist()
+//    - Cache.reload()
+//    - Cache.destroy()
+
+
 
 /**
  * Private in-memory cache storage.
@@ -53,8 +62,8 @@ class CacheConfigurationManagerInterface extends ConfigurationManagerInterface {
 
     this.get = this.get.bind(this);
     this.buildTree = this.buildTree.bind(this);
-    this.getValidConfiguration = this.getValidConfiguration.bind(this);
-    this.getRebuiltCache = this.getRebuiltCache.bind(this);
+    this.getValidConfigurationObject = this.getValidConfigurationObject.bind(this);
+    this.getRebuiltCacheObject = this.getRebuiltCacheObject.bind(this);
     this.writeCache = this.writeCache.bind(this);
     this.getTTL = this.getTTL.bind(this);
     this.prepareCacheObject = this.prepareCacheObject.bind(this);
@@ -63,10 +72,13 @@ class CacheConfigurationManagerInterface extends ConfigurationManagerInterface {
     this.getCache = this.getCache.bind(this);
     this.readCacheFromFile = this.readCacheFromFile.bind(this);
     this.extractCacheData = this.extractCacheData.bind(this);
+    this.readAndSetCacheInMemory = this.readAndSetCacheInMemory.bind(this);
   }
 
+  /**
+   * @inheritdoc
+   */
   get(property) {
-    console.log('cache get method');
     return this.buildTree()
       .then((tree) => {
         return this.nodeSelector.query(tree, property)}
@@ -74,26 +86,29 @@ class CacheConfigurationManagerInterface extends ConfigurationManagerInterface {
   }
 
   /**
-   * Fetch a TTL object to pass on to the logic that will determine if the
-   * config we return is valid.
-   *
-   * @return {<Promise>Hashable}
+   * @inheritdoc
    */
   buildTree() {
     return this.getTTL()
-      .then(this.getValidConfiguration)
+      .then(this.getValidConfigurationObject)
       .then(this.extractCacheData);
   }
 
-  extractCacheData(cache) {
-    return cache.data;
-  }
-
-  getValidConfiguration(ttl) {
+  /**
+   * Get a valid cache object according to the specified TTL.
+   *
+   * @param  {Timer} ttl
+   *  A timer object representing the TTL.
+   *
+   * @return {Cache}
+   *  A cache object.
+   */
+  getValidConfigurationObject(ttl) {
     console.log('seconds remaining: ' + ttl.timeRemaining() / 1000);
+
     if (ttl.isExpired()) {
       console.log('cache expired'.red);
-      return this.getRebuiltCache();
+      return this.getRebuiltCacheObject();
     }
     else {
       console.log('using cached config'.green);
@@ -101,20 +116,33 @@ class CacheConfigurationManagerInterface extends ConfigurationManagerInterface {
     }
   }
 
-  getRebuiltCache() {
-    console.log('rebuilding cache'.red);
+  /**
+   * Rebuild cache and return a cache object from the canonical source.
+   *
+   * @return {Cache}
+   *  A cache object.
+   */
+  getRebuiltCacheObject() {
+    console.log('rebuilding cache object'.red);
     return this.component.buildTree()
       .then(this.writeCache)
-      .then(this.readCacheFromFile)
-      .then(this.setCacheInMemory);
+      .then(this.readAndSetCacheInMemory);
   }
 
+  /**
+   * Save cache in a persistent storage.
+   *
+   * @param  {Hashable} tree
+   *   A configuration object.
+   *
+   * @return {<Promise>Void}
+   */
   writeCache(tree) {
     console.log("writing cache to file".grey);
     // throw new Error('You must provide an implementation for the setCache method.');
-    const preparedObject = this.prepareCacheObject(tree);
+    const cacheObject = this.prepareCacheObject(tree);
 
-    return jsonFile.writeFile(cacheFileName, preparedObject, {spaces: 2, EOL: '\r\n'})
+    return jsonFile.writeFile(cacheFileName, cacheObject, {spaces: 2, EOL: '\r\n'})
       .then( res => {
         console.log("writing complete".grey);
       })
@@ -123,11 +151,19 @@ class CacheConfigurationManagerInterface extends ConfigurationManagerInterface {
       });
   }
 
+  /**
+   * Read cache from persistent storage
+   */
   readCacheFromFile() {
     console.log('Reading cache from file'.grey);
     return jsonFile.readFile(cacheFileName);
   }
 
+  /**
+   * Indiscriminately fetch cache object as it is available. A new cache object
+   * will be created an existing one is found.
+   * @return {[type]} [description]
+   */
   getCache() {
     // throw new Error('You must provide an implementation for the getCache method.');
     if (cache) {
@@ -136,29 +172,42 @@ class CacheConfigurationManagerInterface extends ConfigurationManagerInterface {
     }
     else {
       console.log('No in-memory cache found'.yellow);
-      return this.readCacheFromFile()
-        .then(this.setCacheInMemory)
+      return this.readAndSetCacheInMemory()
+        // If there was an error reading cache from file, then we attempt to
+        // write the rebuild the cache and persist it.
         .catch( error => {
           console.log('no file found, but building anyway...'.grey);
-          return this.component.buildTree()
-            .then(this.writeCache)
-            .then(this.readCacheFromFile)
-            .then(this.setCacheInMemory);
+          return getRebuiltCacheObject()
+            .catch( error => console.error(error));
         });
     }
   }
 
+  /**
+   * Read cache from persistent storage and save in-memory.
+   */
+  readAndSetCacheInMemory(cacheObject) {
+    return this.readCacheFromFile()
+      .then(this.setCacheInMemory);
+  }
+
+  /**
+   * Sets in-memory cache object.
+   */
   setCacheInMemory(fileContents) {
     console.log('Persisting cache in-memory'.grey)
     cache = fileContents;
     return Promise.resolve(cache);
   }
 
+  /**
+   * Deletes in-memory and persistent storage of cache.
+   */
   clearCache() {
     // throw new Error('You must provide an implementation for the clearCache method.');
     console.log("Clearing cache".red);
     cache = undefined;
-    
+
     return new Promise( (resolve, reject) => {
       require('fs').unlink(cacheFileName, error => {
         if (error) {
@@ -169,6 +218,18 @@ class CacheConfigurationManagerInterface extends ConfigurationManagerInterface {
     });
   }
 
+  /**
+   * Helper for extracting the actual data from a cache object.
+   * @return {Hashable}
+   */
+  extractCacheData(cacheObject) {
+    return cacheObject.data;
+  }
+
+  /**
+   * Wraps data into a "cache" object.
+   * @return {Cache}
+   */
   prepareCacheObject(tree) {
     return {
       lastModified: new Date().getTime(),
@@ -176,6 +237,10 @@ class CacheConfigurationManagerInterface extends ConfigurationManagerInterface {
     };
   }
 
+  /**
+   * Returns a Timer instance which is used to represent the cache's TTL.
+   * @return {Timer}
+   */
   getTTL() {
     return this.getCache().then( cache => {
       if (cache && cache.lastModified) {
